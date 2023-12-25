a77356f6a0b0ff7eb1e35e8b203c8b50a50400b9
substicker <yttrium@tuta.io>
2023-12-25 16:35:53 -0300

    añadido input de color, modificado formulario levemente

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,18 +1,18 @@
-import { createStore } from "solid-js/store";
+import { unwrap } from "solid-js/store";
 import { useAppContext } from "../context/AppContext";
 
 const DEFAULT_LIFEEVENT = {
   id: -1,
   title: 'Untitled',
   description: '',
+  eventColor: '',
   startWeek: new Date(),
   endWeek: new Date(),
 }
 
 export default function Aside(){
-  const [option, setOptions] = useAppContext();
-  const [lifeEvents, setLifeEvents] = createStore([]);
-  
+  const [options, setOptions, lifeEvents, setLifeEvents] = useAppContext();
+
   function addLifeEvent() {
     let newLifeEvent = structuredClone(DEFAULT_LIFEEVENT);
     newLifeEvent.id = lifeEvents.length;
@@ -27,13 +27,13 @@ export default function Aside(){
     <aside class="p-6 h-screen border-r border-neutral-400 w-2/12 [&_label]:inline-block [&_label]:w-full [&_input]:border-2 [&_input]:border-neutral-400">
       <div>
         <label for="title">Título</label>
-        <input name="title" type="text" value={option.title} onInput={(e) => {setOptions('title', e.target.value)}}/>
+        <input name="title" type="text" value={options.title} onInput={(e) => {setOptions('title', e.target.value)}}/>
         <label for="description">Descripción</label>
-        <input name="description" type="text" value={option.description} onInput={(e) => {setOptions('description', e.target.value)}}/>
+        <input name="description" type="text" value={options.description} onInput={(e) => {setOptions('description', e.target.value)}}/>
         <label for="born">Año de nacimiento</label>
-        <input name="born" type="date" value={option.born.toISOString().slice(0,-14)} onInput={(e) => {setOptions('born', new Date(e.target.value))}}/>
+        <input name="born" type="date" value={"2005-03-20"} /* value={options.born.toISOString().slice(0,-14)} */ onInput={(e) => {setOptions('born', new Date(e.target.value))}}/>
         <label for="yearsExpected">Expectativa de años de vida</label>
-        <input name="yearsExpected" type="number" max={100} value={option.yearsExpected} onChange={(e) => {setOptions('yearsExpected', Number(e.target.value))}}/>
+        <input name="yearsExpected" type="number" max={100} value={options.yearsExpected} onChange={(e) => {setOptions('yearsExpected', Number(e.target.value))}}/>
         <section class="mt-4 border border-nuetral-400">
           <header>
             <button class="flex justify-between w-full" onClick={() => (addLifeEvent())}>Añadir contexto <img src="add.svg"/></button>
@@ -43,7 +43,7 @@ export default function Aside(){
               <input value={lifeEvent.title} type="text" onChange={(e) => {modifyLifeEvent(lifeEvent.id, 'title', e.target.value)}}/>
               <input value={lifeEvent.startWeek.toISOString().slice(0,-14)} type="date" onChange={(e) => {modifyLifeEvent(lifeEvent.id, 'startWeek', new Date(e.target.value))}} />
               <input value={lifeEvent.endWeek.toISOString().slice(0,-14)} type="date" onChange={(e) => {modifyLifeEvent(lifeEvent.id, 'endWeek', new Date(e.target.value))}} />
-
+              <input value={lifeEvent.eventColor} type="color" onInput={(e) => {modifyLifeEvent(lifeEvent.id, 'eventColor', e.target.value)}}/>
             </div>
           }</For>
         </section>
