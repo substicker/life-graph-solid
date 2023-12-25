7b56246a0ed81019100b16536bd25dfe107a218a
substicker <yttrium@tuta.io>
2023-12-24 21:00:33 -0300

    añadido contexto de opciones

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import { createSignal } from 'solid-js'
 import solidLogo from './assets/solid.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Week from './components/Week'
 import Aside from './components/Aside'
+import Life from './components/Life'
 
 function App() {
  
   return (
     <div class="flex">
       <Aside />
-      <Week />
+      <Life />
     </div>
   )
 }
