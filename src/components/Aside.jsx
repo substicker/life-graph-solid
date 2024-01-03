dab45ed070df03bc514a8ba2098cd08f6449b085
substicker <yttrium@tuta.io>
2024-01-03 09:55:52 -0300

    creado apartado de información

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -9,9 +9,15 @@ const DEFAULT_LIFEEVENT = {
   endWeek: new Date(),
 }
 
+function clickOutside(el, accessor) {
+  const onClick = (e) => !el.contains(e.target) && accessor()?.();
+  document.body.addEventListener("click", onClick);
+
+  onCleanup(() => document.body.removeEventListener("click", onClick));
+}
+
 export default function Aside(){
   const [options, setOptions, lifeEvents, setLifeEvents] = useAppContext();
-
   function addLifeEvent() {
     let newLifeEvent = structuredClone(DEFAULT_LIFEEVENT);
     newLifeEvent.id = lifeEvents.length;
@@ -31,6 +37,17 @@ export default function Aside(){
     <aside class="p-6 h-screen border-r border-neutral-400 w-2/12 [&_label]:inline-block [&_label]:w-full [&_input]:border-2 [&_input]:border-neutral-400">
       <div>
         <header>
+          <div>
+            <details>
+              <summary class="list-none overflow-hidden transition-all">
+              <img src="info.svg" class="cursor-pointer" />
+              </summary>
+              <section class="text-center overflow-hidden transition-all">
+                <h1 class="text-2xl pt-4">Life Graph v1.0.4</h1>
+                <h2 class="pb-4">Hecho para un regalo</h2>
+              </section>
+            </details>
+          </div>
           <p><b>Importante:</b> esta aplicación <span class="text-red-500">NO ALMACENA NADA</span>. Eventualmente será agregado.</p>
         </header>
         <hr class="my-4" />
