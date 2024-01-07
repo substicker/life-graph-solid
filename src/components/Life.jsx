ac8b656c391435b208aa625b961f4f1c2cab6c42
substicker <yttrium@tuta.io>
2024-01-07 11:10:14 -0300

    eliminado título

--- src/components/Life.jsx
+++ src/components/Life.jsx
@@ -6,7 +6,6 @@ export default function Life(props){
   return (
     <main class="w-full flex flex-col items-center">
       <header class="text-center my-10">
-        <h1 class="text-4xl">{options.title == "Untitled" ? "" : options.title}</h1>
         <h2 class="text-2xl font-light">{options.description}</h2>
       </header>
       <section>
